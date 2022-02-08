20fe12cdb7d4a231aa6cc3d3c554f443cc05ddfd
Oscar Mulder <omulder@student.codam.nl>
2022-02-08 16:21:13 +0100

    fix leak in checker on enter

--- srcs-checker/checker.c
+++ srcs-checker/checker.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/01/14 10:30:57 by omulder       #+#    #+#                 */
-/*   Updated: 2020/03/08 14:11:32 by omulder       ########   odam.nl         */
+/*   Updated: 2022/02/08 16:20:58 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,8 +58,10 @@ static int	read_and_do_opps(t_stack **a, t_stack **b, int *op_count)
 	while (ret == 1)
 	{
 		ret = get_next_line(STDIN_FILENO, &line);
-		if (*op_count == 0 && ret == 1 && *line == '\0')
+		if (*op_count == 0 && ret == 1 && *line == '\0') {
+			ft_strdel(&line);
 			return (1);
+		}
 		if (ret > 0)
 		{
 			if (!do_opp(a, b, line))
